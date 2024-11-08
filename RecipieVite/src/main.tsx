303bbb339410ce14ddbf6e3e6eb51189192999da
aaronforsnow <twodoschool@gmail.com>
2024-11-08 09:48:28 -0700

    init vite, with part of auth

---
+++ RecipieVite/src/main.tsx
@@ -0,0 +1,52 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { AuthProvider, useAuth } from "react-oidc-context";
+import './index.css'
+import App from './App.tsx'
+
+const oidcConfig = {
+  authority: "<your authority>",
+  client_id: "<your client id>",
+  redirect_uri: "<your redirect uri>",
+  // ...
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AuthProvider {...oidcConfig}>
+    <LogIn/>
+    <App />
+    </AuthProvider>
+  </StrictMode>,
+)
+
+
+export function LogIn() {
+    const auth = useAuth();
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+        <div>
+            Hello {auth.user?.profile.sub}{" "}
+            <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+        );
+    }
+
+    return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+}
