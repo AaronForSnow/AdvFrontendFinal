3d449dc7dfde59c3a557d640dd38da3c0818f173
aaronforsnow <twodoschool@gmail.com>
2024-11-30 10:23:42 -0700

    added type to user

--- RecipieVite/src/main.tsx
+++ RecipieVite/src/main.tsx
@@ -5,13 +5,15 @@ import "./Styles/custom.scss";
 import "bootstrap";
 import App from "./App.tsx";
 import { IngredientContextProvider } from "./context/IngredientContextProvider.tsx";
+import { User } from "oidc-client-ts";
 
 const oidcConfig = {
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
   client_id: "aaron-final",
   redirect_uri: import.meta.env.VITE_REDIRECT_URL,
-  onSigninCallback: (user)=> {
+  onSigninCallback: (user: User | undefined)=> {
     console.log(user)
+    console.log(user?.profile.email)
     window.history.replaceState({}, document.title, window.location.pathname)
   },
   // ...
