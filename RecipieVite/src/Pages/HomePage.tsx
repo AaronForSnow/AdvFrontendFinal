f71bccb503f765a718c349fa8b33ef8f69cd5285
aaronforsnow <twodoschool@gmail.com>
2024-12-04 22:28:59 -0700

    added local storage

--- RecipieVite/src/Pages/HomePage.tsx
+++ RecipieVite/src/Pages/HomePage.tsx
@@ -1,11 +1,32 @@
-import { useIngredientContext } from "../context/useIngredientContextProvider"
+import { useEffect, useState } from "react";
+import { useIngredientContext } from "../context/useIngredientContextProvider";
 
-export function HomePage () {
-    const {successToast } = useIngredientContext();
-    return (
-        <>
-            <div>this is the HomePage</div>
-            <button onClick={() => successToast("this is visible!!!!")}> make toast</button>
-        </>
-    )
+export function HomePage() {
+  const { successToast } = useIngredientContext();
+  const [locString, setLocString] = useState<string>("");
+  const saveToLocalStorage = (value: string) => {
+    localStorage.setItem("myData", value);
+    setLocString(value); // Update state
+  };
+  useEffect(() => {
+    const storedData = localStorage.getItem("myData");
+    if (storedData) {
+      setLocString(storedData); // Set the state with the value from localStorage
+    }
+  }, []);
+  return (
+    <>
+      <div>this is the HomePage</div>
+      <button onClick={() => successToast("this is visible!!!!")}>
+        {" "}
+        make toast
+      </button>
+
+      <p>Stored Data: {locString}</p>
+      <input></input>
+      <button onClick={() => saveToLocalStorage("Hello, Aaron this is you")}>
+        Save Data
+      </button>
+    </>
+  );
 }
